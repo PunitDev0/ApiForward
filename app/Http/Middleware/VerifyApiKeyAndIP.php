980558d73e59a3c0354b3a38efa83e41cee0e6d6
PunitDev0 <punitdeveloper1@gmail.com>
2025-04-11 16:00:22 +0530

    fixed ip error

--- app/Http/Middleware/VerifyApiKeyAndIP.php
+++ app/Http/Middleware/VerifyApiKeyAndIP.php
@@ -24,7 +24,7 @@ class VerifyApiKeyAndIP
             return response()->json(['error' => 'Invalid API key'], 401);
         }
 
-        if ($recordIP) {
+        if (!$recordIP) {
             return response()->json(['error' => 'IP address not allowed'], 403);
         }
 
